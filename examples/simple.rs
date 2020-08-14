b2f98bc79bc526d834e8fa8de1e6c9014148cbfb
Hunter Damron <hdamron1594@yahoo.com>
2020-08-14 18:42:20 -0400

    Implemented basic functionality

--- examples/simple.rs
+++ examples/simple.rs
@@ -5,6 +5,14 @@ fn main() {
     let transform = IsometryMatrix3::translation(0.0, 1.0, 3.0);
     let mut tree = mulberry::Tree::<&str, IsometryMatrix3<f32>>::default();
     tree.set_transform(transform, "/f1", "/f2");
-    let inv_transform = tree.get_direct_transform("/f2", "/f1");
-    dbg!(inv_transform);
+    tree.set_transform(transform, "/f3", "/f2");
+    tree.set_transform(transform, "/f4", "/f3");
+    tree.set_transform(transform, "/f5", "/f2");
+    tree.set_transform(transform, "/f6", "/f4");
+
+    dbg!(tree.get_path("/f6", "/f5"));
+    dbg!(tree.get_path("/f1", "/f1"));
+    dbg!(tree.get_path("/fake", "/fake"));
+    dbg!(tree.get_path("/f1", "/fake"));
+    dbg!(tree.get_transform("/f6", "/f5"));
 }
--- src/tree.rs
+++ src/tree.rs
@@ -28,6 +28,10 @@ impl<K, T> Tree<K, T> {
 }
 
 impl<K: Eq + Hash + Clone, T: Group + Clone> Tree<K, T> {
+    pub fn has_frame(&self, key: &K) -> bool {
+        self.keys.contains(key)
+    }
+
     pub fn set_transform(&mut self, transform: T, from_key: K, to_key: K) {
         let from_to_key = (from_key, to_key);
         if self.transforms.contains_key(&from_to_key) {
@@ -56,11 +60,76 @@ impl<K: Eq + Hash + Clone, T: Group + Clone> Tree<K, T> {
         }
     }
 
-    pub fn get_direct_transform(&self, from_key: K, to_key: K) -> Option<T> {
+    fn get_ancestry(&self, mut key: K) -> Vec<K> {
+        let mut ret: Vec<_> = std::iter::from_fn(|| {
+            self.parents.get(&key).map(|newkey| {
+                let mut newkey = newkey.clone();
+                std::mem::swap(&mut key, &mut newkey);
+                newkey
+            })
+        })
+        .collect();
+        ret.push(key); // Add last key
+        ret
+    }
+
+    fn get_root(&self, key: K) -> K {
+        self.get_ancestry(key).last().unwrap().clone() // One element guaranteed
+    }
+
+    pub fn can_transform(&self, from_key: K, to_key: K) -> bool {
+        self.get_root(from_key) == self.get_root(to_key)
+    }
+
+    pub fn get_path(&self, from_key: K, to_key: K) -> Option<Vec<K>> {
+        if from_key == to_key {
+            return Some(vec![from_key]);
+        }
+
+        let mut from_root_path = self.get_ancestry(from_key).into_iter().rev().peekable();
+        let mut to_root_path = self.get_ancestry(to_key).into_iter().rev().peekable();
+        let mut center = None; // Connecting frame
+
+        // The paths cannot be the same since `from_key` != `to_key`, hence no infinite loop
+        while from_root_path.peek() == to_root_path.peek() {
+            center = from_root_path.next();
+            to_root_path.next();
+        }
+
+        Some(
+            from_root_path
+                .rev() // Go up `from_key` up to `center`
+                .chain(std::iter::once(center?)) // Include `center` if there was a connection
+                .chain(to_root_path) // `Go down `center` to `to_key`
+                .collect(),
+        )
+    }
+
+    fn get_direct_transform(&self, from_key: K, to_key: K) -> Option<T> {
         let from_to_key = (from_key, to_key);
         self.transforms.get(&from_to_key).map(T::clone).or_else(|| {
-            let to_from_key = (from_to_key.1, from_to_key.0);
+            let to_from_key = (from_to_key.1, from_to_key.0); // Try the other direction
             self.transforms.get(&to_from_key).map(T::inverse)
         })
+    }
+
+    fn get_transform_from_path(&self, path: Vec<K>) -> T {
+        let (head, tail) = match path.split_first() {
+            Some((head, tail)) => (head, tail),
+            None => return T::identity(),
+        };
+        tail.into_iter()
+            .fold((T::identity(), head), |(acc, prevkey), key| {
+                let transform = self
+                    .get_direct_transform(prevkey.clone(), key.clone())
+                    .expect("Invalid path"); // TODO return a recoverable error
+                (acc.compose(&transform), key)
+            })
+            .0
+    }
+
+    pub fn get_transform(&self, from_key: K, to_key: K) -> Option<T> {
+        let path = self.get_path(from_key, to_key)?;
+        Some(self.get_transform_from_path(path))
     }
 }
