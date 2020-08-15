218fb4f1932c66e42eb8c6c7cb53618619df49da
Hunter Damron <hdamron1594@yahoo.com>
2020-08-14 20:21:39 -0400

    Implement tree connecting and DOT output

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,14 +1,25 @@
 extern crate nalgebra as na;
-use na::IsometryMatrix3;
+use na::{IsometryMatrix2, Vector2};
 
 fn main() {
-    let transform = IsometryMatrix3::translation(0.0, 1.0, 3.0);
-    let mut tree = mulberry::Tree::<&str, IsometryMatrix3<f32>>::default();
-    tree.set_transform(transform, "/f1", "/f2");
-    tree.set_transform(transform, "/f3", "/f2");
-    tree.set_transform(transform, "/f4", "/f3");
-    tree.set_transform(transform, "/f5", "/f2");
-    tree.set_transform(transform, "/f6", "/f4");
+    let tf = |x, y, rot| IsometryMatrix2::new(Vector2::new(x, y), rot);
+
+    let mut tree = mulberry::Tree::<&str, IsometryMatrix2<f32>>::default();
+
+    tree.set_transform(tf(1.0, 0.0, 3.14), "/f1", "/f2");
+    tree.set_transform(tf(1.0, 0.0, 0.0), "/f3", "/f2");
+    tree.set_transform(tf(2.0, 1.0, 3.14 / 2.), "/f3", "/f4");
+    tree.set_transform(tf(5.0, 3.0, 3.14 / 2.), "/f5", "/f3");
+
+    tree.set_transform(tf(0.0, 7.0, 0.0), "/f6", "/f7");
+    tree.set_transform(tf(0.0, 7.0, 0.0), "/f6", "/f8");
+    tree.set_transform(tf(0.0, 7.0, 0.0), "/f8", "/f9");
+
+    println!("{}", tree.output_dot("separate"));
+
+    tree.set_transform(tf(0.0, 7.0, 0.0), "/f9", "/f5");
+
+    println!("{}", tree.output_dot("connected"));
 
     dbg!(tree.get_path("/f6", "/f5"));
     dbg!(tree.get_path("/f1", "/f1"));
